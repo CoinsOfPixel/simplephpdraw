c747427e99b51dbc354a47538375ce39ea567dc8
CoinsOfPixel <30437560+CoinsOfPixel@users.noreply.github.com>
2020-11-25 10:12:11 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+     <link rel="stylesheet" href="css/styles.css">
+    <title>DRAW</title>
+  </head>
+
+  <body>
+
+      <div class="receiver-add-field">
+        <form action="result.php" method="post">
+                <p>■ INPUT THE NAME OF THE USERS IN THE DRAW:</p>
+        <textarea name="draw" rows="30" cols="20"></textarea>
+      </div>
+      <div class="submit_to_draw">
+        <input type="submit" value="DRAW">
+      </div>
+  </body>
+</html>
